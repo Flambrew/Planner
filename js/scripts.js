89dfb545b23d533ef9960e03002e78d5fce94ae7
Flambrew <77941661+Flambrew@users.noreply.github.com>
2023-02-16 16:50:00 -0500

    gah

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+function assembleCalendar() {
+    for (let i = 0; i < 7; i++) {
+        let line = document.getElementsByClassName("calendar-line").item(i).appendChild()
+    }
+}
+
+function selectDay(index) {
+
+}
